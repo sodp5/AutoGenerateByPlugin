1e53e6947ba76a0b494d9de3273dc58184f41242
dean <sodp5@naver.com>
2022-09-02 18:39:03 +0900

    annotation 붙어있는 클래스 읽어오도록 변경

--- src/main/kotlin/com/github/sodp5/intellijpluginsample/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/sodp5/intellijpluginsample/listeners/MyProjectManagerListener.kt
@@ -1,6 +1,7 @@
 package com.github.sodp5.intellijpluginsample.listeners
 
 import com.github.sodp5.intellijpluginsample.services.MyProjectService
+import com.github.sodp5.intellijpluginsample.util.AnnotationSearchUtil
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
--- src/main/kotlin/com/github/sodp5/intellijpluginsample/services/LauncherCache.kt
+++ src/main/kotlin/com/github/sodp5/intellijpluginsample/services/LauncherCache.kt
@@ -1,6 +1,7 @@
 package com.github.sodp5.intellijpluginsample.services
 
 import com.github.sodp5.intellijpluginsample.psi.LightLauncherClass
+import com.github.sodp5.intellijpluginsample.util.AnnotationSearchUtil
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiManager
@@ -13,6 +14,15 @@ open class LauncherCache(
     }
 
     fun getClasses(): List<PsiClass> {
+        val annotationFqn = "com.munny.dummyproject.annotations.Launcher"
+        val annotationClasses = AnnotationSearchUtil.getAnnotatedElements(project, annotationFqn)
+
+        annotationClasses.forEach { ktDeclaration ->
+            val name = ktDeclaration.name ?: return@forEach
+            println(ktDeclaration)
+            println(ktDeclaration.name)
+        }
+
         return classes
     }
 }
